1bb09a508b115158f714161f81c40926eff994a3
hanbyul1025@gmail.com <hanbyul1025@gmail.com>
2023-08-23 09:01:55 +0900

    design: 그리드 반응형으로 적용

--- src/components/ideaPage/IdeaList.tsx
+++ src/components/ideaPage/IdeaList.tsx
@@ -84,7 +84,7 @@ const data = [
 export default function IdeaList() {
   return (
     <div className="flex flex-col items-center gap-[7.5rem] mb-20">
-      <div className="grid grid-cols-4 mt-20 gap-x-4 gap-y-10">
+      <div className="grid md:grid-cols-2  lg:grid-cols-3 xl:grid-cols-4 mt-20 gap-x-4 gap-y-10">
         {data.map(({ id, userName, date, title, desc, animal, likes }) => (
           <Link href={`/idea/${id}`} className="flex flex-col w-72 " key={id}>
             <div className="bg-sub2 rounded-t-lg text-center relative h-[250px]">
