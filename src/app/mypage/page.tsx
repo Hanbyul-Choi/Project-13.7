b5246bcb0c122825dccfedf2f3e7a386371bbba9
YJ Baek <yjbaek29@gmail.com>
2023-08-24 21:39:09 +0900

    feat: 마이페이지 get 필요 데이터, chore: asset 폴더 생성, design: 프로필 default image 추가 in asset폴더

--- src/app/mypage/page.tsx
+++ src/app/mypage/page.tsx
@@ -1,10 +1,13 @@
 'use client';
 import React, { useEffect, useState } from 'react';
 
+import Image from 'next/image';
+
 import Button from '@/components/common/Button';
 import useSessionStore from '@/store';
 
 import { supabase } from '../../../supabase/supabaseConfig';
+import profileDefaultImg from '../../assets/profileDefaultImg.png';
 
 import type { Props } from '@/types/PropsType';
 
@@ -13,6 +16,23 @@ type UserProfile = {
   email: string;
   nickname: string;
   point: number;
+  profile_img: string | null;
+};
+
+type UserChallenges = {
+  mainChallenge: any;
+  id: string;
+  title: string;
+  startDate: number;
+  endDate: number;
+  isCompleted: boolean;
+};
+
+type UserReviews = {
+  mainChallenge: any;
+  id: string;
+  created_at: number;
+  insta_url: string;
 };
 
 export default function Page({ params: { slug } }: Props) {
@@ -21,11 +41,15 @@ export default function Page({ params: { slug } }: Props) {
     email: '',
     nickname: '',
     point: 0,
+    profile_img: '',
   });
 
-  const session = useSessionStore((state: { session: any }) => state.session);
-  // console.log(session?.user.id);
+  const [userChallenges, setUserChallenges] = useState<UserChallenges[]>([]);
+  const [userReviews, setUserReviews] = useState<UserReviews[]>([]);
 
+  const session = useSessionStore((state: { session: any }) => state.session);
+
+  // user profile
   const loadUserInfo = async () => {
     let { data: users } = await supabase.from('users').select('*').eq('user_id', session?.user.id);
     console.log('Users:', users);
@@ -37,18 +61,29 @@ export default function Page({ params: { slug } }: Props) {
         email: user.email,
         nickname: user.nickname,
         point: user.point,
+        profile_img: user.profile_img,
       });
     }
   };
 
+  // user가 신청하고 참여한 main challenge
   const loadUserChallenges = async () => {
-    let { data: challenges } = await supabase.from('joinChallenge').select(`*, mainChallenge (*)`).eq('user_id', session?.user.id);
+    let { data: challenges } = await supabase.from('joinChallenge').select(`*, mainChallenge (*)`);
     console.log('Challenges:', challenges);
+
+    if (challenges) {
+      setUserChallenges(challenges);
+    }
   };
 
+  // user의 챌린지 인증 게시글 불러오기
   const loadUserReviews = async () => {
-    let { data: reviews } = await supabase.from('reviews').select('*').eq('user_id', session?.user.id);
+    let { data: reviews } = await supabase.from('reviews').select(`*, mainChallenge (title)`).eq('user_id', session?.user.id);
     console.log('Reviews:', reviews);
+
+    if (reviews) {
+      setUserReviews(reviews);
+    }
   };
 
   useEffect(() => {
@@ -77,20 +112,51 @@ export default function Page({ params: { slug } }: Props) {
 
   return (
     <div>
+      {/* 유저프로필 */}
       <div>
         {slug}
         <h1>My Page</h1>
+        <Image src={userProfile?.profile_img ? `${userProfile.profile_img}` : profileDefaultImg} alt="profileDefaultImg" className="w-[100px] h-[100px] rounded-full inline-block mb-4" />
+        <Button btnType={'black'} size={'small'}>
+          내정보 수정하기
+        </Button>
         <p>Nickname: {userProfile.nickname}</p>
         <p>Email: {userProfile.email}</p>
       </div>
-      <p>내 뱃지</p>
+      <h1>내 뱃지</h1>
+      <div>---------------뱃지영역---------------</div>
+
+      {/* 나무충전 */}
       <div>
-        <p>현재 나무 총{userProfile.point}그루</p>
-        <Button btnType={'green'} size={'medium'}>
+        <h1>현재 나무 총{userProfile.point}그루</h1>
+        <Button btnType={'green'} size={'large'}>
           나무 충전하기
         </Button>
       </div>
-      <div>{}</div>
+
+      {/* 참여중인 챌린지 */}
+      <h1>내 챌린지</h1>
+      {userChallenges?.map((item, index) => (
+        <div key={item.id}>
+          <div>{index + 1}</div>
+          <div>제목: {item.mainChallenge.title}</div>
+          <div>시작일: {item.mainChallenge.startDate}</div>
+          <div>종료일: {item.mainChallenge.endDate}</div>
+        </div>
+      ))}
+
+      {/* 챌린지 인증 게시글 리스트 */}
+      <h1>내 챌린지 인증</h1>
+      <div>
+        {userReviews?.map((item, index) => (
+          <div key={item.id}>
+            <div>{index + 1}</div>
+            <div>제목: {item.mainChallenge.title}</div>
+            <div>시작일: {item.insta_url}</div>
+            <div>종료일: {item.created_at}</div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
