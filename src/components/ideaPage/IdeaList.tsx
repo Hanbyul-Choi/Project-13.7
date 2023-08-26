7c8aaddd431db27e2a43807f1a79ce910bec2281
hanbyul1025@gmail.com <hanbyul1025@gmail.com>
2023-08-26 12:00:13 +0900

    chore: 파일 임시저

--- src/components/ideaPage/IdeaList.tsx
+++ src/components/ideaPage/IdeaList.tsx
@@ -1,118 +1,69 @@
-import React from 'react';
+'use client';
+import React, { useState } from 'react';
 
 import Image from 'next/image';
 import Link from 'next/link';
 import { AiOutlineArrowRight } from 'react-icons/ai';
+import { useQuery } from 'react-query';
+import { PropagateLoader } from 'react-spinners';
 
+import { getSuggestions } from '@/app/api/ChallengeSuggestion';
+
+import IdeaHeader from './IdeaHeader';
+import Dog from '../../../public/dog.jpg';
 import heartIcon from '../../../public/heart.svg';
 import Button from '../common/Button';
 
-const data = [
-  {
-    id: 1,
-    userName: 'userName',
-    date: Date.now(),
-    title: '모두 함께 일회용품 사용을 줄여보아요 글자잘림 테스트 글자잘림 테스트',
-    desc: '텀블러 사용으로 일회용컵 쓰레기 줄이기 챌린지 도전해요! ',
-    animal: '북극곰',
-    likes: 1234,
-  },
-  {
-    id: 2,
-    userName: 'userName',
-    date: Date.now(),
-    title: '모두 함께 일회용품 사용을 줄여보아요 글자잘림 테스트 글자잘림 테스트',
-    desc: '텀블러 사용으로 일회용컵 쓰레기 줄이기 챌린지 도전해요! ',
-    animal: '북극곰',
-    likes: 1234,
-  },
-  {
-    id: 3,
-    userName: 'userName',
-    date: Date.now(),
-    title: '모두 함께 일회용품 사용을 줄여보아요 글자잘림 테스트 글자잘림 테스트',
-    desc: '텀블러 사용으로 일회용컵 쓰레기 줄이기 챌린지 도전해요! ',
-    animal: '북극곰',
-    likes: 1234,
-  },
-  {
-    id: 4,
-    userName: 'userName',
-    date: Date.now(),
-    title: '모두 함께 일회용품 사용을 줄여보아요 글자잘림 테스트 글자잘림 테스트',
-    desc: '텀블러 사용으로 일회용컵 쓰레기 줄이기 챌린지 도전해요! 트',
-    animal: '북극곰',
-    likes: 1234,
-  },
-  {
-    id: 5,
-    userName: 'userName',
-    date: Date.now(),
-    title: '모두 함께 일회용품 사용을 줄여보아요 글자잘림 테스트 글자잘림 테스트',
-    desc: '텀블러 사용으로 일회용컵 쓰레기 줄이기 챌린지 도전해요! ',
-    animal: '북극곰',
-    likes: 1234,
-  },
-  {
-    id: 6,
-    userName: 'userName',
-    date: Date.now(),
-    title: '모두 함께 일회용품 사용을 줄여보아요 글자잘림 테스트 글자잘림 테스트',
-    desc: '텀블러 사용으로 일회용컵 쓰레기 줄이기 챌린지 도전해요!',
-    animal: '북극곰',
-    likes: 1234,
-  },
-  {
-    id: 7,
-    userName: 'userName',
-    date: Date.now(),
-    title: '모두 함께 일회용품 사용을 줄여보아요 글자잘림 테스트 글자잘림 테스트',
-    desc: '텀블러 사용으로 일회용컵 쓰레기 줄이기 챌린지 도전해요!',
-    animal: '북극곰',
-    likes: 1234,
-  },
-  {
-    id: 8,
-    userName: 'userName',
-    date: Date.now(),
-    title: '모두 함께 일회용품 사용을 줄여보아요 글자잘림 테스트 글자잘림 테스트',
-    desc: '텀블러 사용으로 일회용컵 쓰레기 줄이기 챌린지 도전해요!',
-    animal: '북극곰',
-    likes: 1234,
-  },
-];
+export type SortWay = '추천순' | '최신순';
 
 export default function IdeaList() {
+  const [sortWay, setSortway] = useState<SortWay>('추천순');
+  let { isError, data } = useQuery('challengeSuggestion', getSuggestions);
+
+  if (isError) {
+    return <p>에러가 발생했습니다. 새로고침 해주세요.</p>;
+  }
+
   return (
-    <div className="flex flex-col items-center gap-[7.5rem] mb-20">
-      <div className="grid md:grid-cols-2  lg:grid-cols-3 xl:grid-cols-4 mt-20 gap-x-4 gap-y-10">
-        {data.map(({ id, userName, date, title, desc, animal, likes }) => (
-          <Link href={`/idea/${id}`} className="flex flex-col w-72 " key={id}>
-            <div className="bg-sub2 rounded-t-lg text-center relative h-[250px]">
-              {/* 이미지 태그로 변경 필요 */}
-              <div>이미지 영역</div>
-              <div className="absolute text-green bottom-4 right-4 flex flex-col items-center rounded-lg bg-white px-3 py-2">
-                <Image priority src={heartIcon} alt="Like this idea" />
-                <p className="text-sm ">{likes}</p>
-              </div>
+    <>
+      {data ? (
+        <>
+          <IdeaHeader sortWay={sortWay} setSortway={setSortway} />
+          <div className="flex flex-col items-center gap-[7.5rem] mb-20">
+            <div className="grid md:grid-cols-2  lg:grid-cols-3 xl:grid-cols-4 mt-20 gap-x-4 gap-y-10">
+              {data.map(({ post_id, users, created_at, title, content, animal, likes }) => (
+                <div className="flex flex-col w-72 " key={post_id}>
+                  <div className="bg-sub2 rounded-t-lg text-center relative h-[250px] overflow-hidden">
+                    <Image src={Dog} alt="개야" style={{ objectFit: 'fill', height: '250px' }} />
+                    <button className="absolute text-green bottom-4 right-4 flex flex-col items-center rounded-lg bg-white px-3 py-2">
+                      <Image priority src={heartIcon} alt="Like this idea" />
+                      <p className="text-sm ">{likes}</p>
+                    </button>
+                  </div>
+                  <Link href={`/idea/${post_id}`} className="flex flex-col px-3 py-6 rounded-b-lg shadow-lg ">
+                    <p className="text-sm opacity-50 ">
+                      {users.nickname} | {new Date(created_at).toLocaleDateString()}
+                    </p>
+                    <p className="mt-3 w-full text-lg font-bold text-ellipsis overflow-hidden whitespace-nowrap">{title}</p>
+                    <p className="mt-2 w-full h-12 opacity-50">{content}</p>
+                    <div className="max-w-fit px-4 py-1 rounded bg-lightblue mt-4">
+                      <p className="text-blue text-sm ">{animal || '북극곰'}을 위한 챌린지</p>
+                    </div>
+                  </Link>
+                </div>
+              ))}
             </div>
-            <div className="flex flex-col px-3 py-6 rounded-b-lg shadow-lg ">
-              <p className="text-sm opacity-50 ">
-                {userName} | {new Date(date).toLocaleDateString()}
-              </p>
-              <p className="mt-3 w-full text-lg font-bold text-ellipsis overflow-hidden whitespace-nowrap">{title}</p>
-              <p className="mt-2 w-full opacity-50">{desc}</p>
-              <div className="max-w-fit px-4 py-1 rounded bg-lightblue mt-4">
-                <p className="text-blue text-sm ">{animal}을 위한 챌린지</p>
-              </div>
-            </div>
-          </Link>
-        ))}
-      </div>
-      <Button btnType="borderBlack" size="large" rounded>
-        더보기
-        <AiOutlineArrowRight size={20} />
-      </Button>
-    </div>
+            <Button btnType="borderBlack" size="large" rounded>
+              더보기
+              <AiOutlineArrowRight size={20} />
+            </Button>
+          </div>
+        </>
+      ) : (
+        <div className="w-full flex justify-center mt-10">
+          <PropagateLoader color="#36d7b7" size={20} />
+        </div>
+      )}
+    </>
   );
 }
