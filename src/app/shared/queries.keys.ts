b522425946d8e747e144ac09891637e402162b9f
hanbyul1025@gmail.com <hanbyul1025@gmail.com>
2023-09-06 09:48:00 +0900

    refactor: query key 통일

---
+++ src/app/shared/queries.keys.ts
@@ -0,0 +1,6 @@
+export const CHALLENGE_SUGGESTION = 'challengeSuggestion_with_users';
+export const MAIN_CHALLENGE = 'on_going_challenge';
+export const REVIEWS = 'challenge_certify';
+export const JOIN_CHALLENGE = 'curUser_join_check';
+export const IDEA_COMMENTS = 'idea_comments';
+export const NATURESTORY = 'nature_story';
