e75fb9db5d77b7dc0fd9dc5b572b582f7c131a2c
hanbyul1025@gmail.com <hanbyul1025@gmail.com>
2023-09-06 19:23:05 +0900

    refactor: 챌린지 신청여부 데이터 관련 쿼리에  옵션 설정

--- src/components/challenge-post/MainChallengeData.tsx
+++ src/components/challenge-post/MainChallengeData.tsx
@@ -4,7 +4,6 @@ import Image from 'next/image';
 
 import { mainChallengeCheck } from '@/app/api/main-challenge';
 import { MAIN_CHALLENGE } from '@/app/shared/queries.keys';
-import useSessionStore from '@/store/sesson.store';
 
 import JoinChallenge from './JoinChallenge';
 import howtojoin from '../../../public/how-to-join.png';
@@ -12,7 +11,7 @@ import SingleLayout from '../layout/SingleLayout';
 
 export default function MainChallengeData() {
   const { data: mainChallenge } = useQuery({ queryKey: [MAIN_CHALLENGE], queryFn: mainChallengeCheck });
-  const { session } = useSessionStore();
+
   if (!mainChallenge) return;
   return (
     <>
@@ -25,7 +24,7 @@ export default function MainChallengeData() {
           <h5 className="mb-4 flex items-center before:content-[' '] before:w-[2px] before:bg-blue before:h-[18px] before:inline-block before:mr-[8px]">참여방법</h5>
           <Image src={howtojoin} alt="how to join challenge" />
           <p className="my-8 text-nagative text-sm">*반드시 인증사진과 해시태그에 ‘#13.7챌린지’가 포함 돼 있어야 합니다.</p>
-          <JoinChallenge session={session} mainChallenge={mainChallenge} />
+          <JoinChallenge mainChallenge={mainChallenge} />
         </div>
       </SingleLayout>
     </>
