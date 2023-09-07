89ee37e34dda9f46bc426c13262189bf6b06fae4
Sunwoo <dlatjsdn205@Naver.com>
2023-09-07 16:16:03 +0900

    Fix: puppeteer config-userDataDir change

--- puppeteer.rc.cjs
+++ puppeteer.rc.cjs
@@ -7,5 +7,5 @@ module.exports = {
   // Puppeteer 실행 옵션을 설정합니다.
   headless: true, // Headless 모드로 실행할지 여부
   args: ['--no-sandbox', '--disable-setuid-sandbox'], // Chromium 실행 옵션 (보안 설정)
-  userDataDir: join(dirname(__dirname, 'src', 'app', 'api'), '.cache', 'puppeteer'), // 캐시 디렉터리 설정
+  userDataDir: join(('src', 'app', 'api'), '.cache', 'puppeteer'), // 캐시 디렉터리 설정
 };
