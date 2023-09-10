4e82f95c4d2151aa6b4379359cdb4a57d55ecbfd
YJ Baek <yjbaek29@gmail.com>
2023-09-11 00:39:36 +0900

    Design: 배너 반응형 수정

--- src/app/nature-story/page.tsx
+++ src/app/nature-story/page.tsx
@@ -6,9 +6,14 @@ import { Content, SideBar } from '@/components/nature-story';
 export default function NaturePage({ searchParams }: { searchParams: { category: string | undefined } }) {
   return (
     <>
-      <div
-        className={`w-full sm:h-[400px] h-[600px] bg-[#white] sm:bg-[url('/nature-banner.png')] bg-[url('/nature-banner-mo.png')] bg-no-repeat bg-center border-t-2 border-t-black/.5 mb-6`}
-      />
+      <div className="px-100w-full mx-auto md:max-w-[77rem] border-y-[1px]">
+        <div
+          className={`float-left ml-2 w-full md:h-[400px] h-[300px] bg-[#white] md:bg-[url('/nature-banner-text.png')] bg-[url('/banner-text-mo.png')] bg-no-repeat bg-left mb-6`}
+        />
+        <div
+          className={`w-full md:h-[400px] h-[600px] bg-[#white] md:bg-[url('/nature-banner.png')] bg-[url('/nature-banner-mo.png')] bg-no-repeat bg-center mb-6`}
+        />
+      </div>
       <Layout>
         <div className="w-full h-[1px] bg-opacityblack mt-10" />
         <section className="flex gap-8 mt-20">
