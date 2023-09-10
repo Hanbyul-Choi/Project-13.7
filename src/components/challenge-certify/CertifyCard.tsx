3a56c908708641ea60518d265db6dace41505839
hanbyul1025@gmail.com <hanbyul1025@gmail.com>
2023-09-10 22:54:13 +0900

    design: 모달 반응형 수정

--- src/components/challenge-certify/CertifyCard.tsx
+++ src/components/challenge-certify/CertifyCard.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 
 // import type { CertifyType } from '@/types/db.type';
-import Link from 'next/link';
 
 import type { Tables } from '@/types/supabase.type';
 
@@ -12,7 +11,7 @@ type CertifyCardProps = {
 const CertifyCard = ({ post }: CertifyCardProps) => {
   return (
     <div className=" my-masonry-grid-column max-h-[600px] relative group overflow-hidden hover:scale-105">
-      <Link href={post.insta_url}>
+      <div>
         <img src={post.img_url ?? ''} alt="" className="rounded-lg object-fit" />
         <div className="flex z-10 w-full bg-white absolute bottom-0 opacity-0 translate-y-full group-hover:translate-y-0 group-hover:opacity-70 transition duration-500 ease-in-out">
           <h2 className="text-lg">{(post as any).users.nickname}</h2>
@@ -21,7 +20,7 @@ const CertifyCard = ({ post }: CertifyCardProps) => {
           &nbsp;
           <h1 className="text-lg">{post.created_at.toString().slice(0, 10)}</h1>
         </div>
-      </Link>
+      </div>
     </div>
   );
 };
